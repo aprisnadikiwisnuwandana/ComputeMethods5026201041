017310d8164993a25ff70d69451b22227abee2ba
aprisnadikiwisnuwandana <81064858+aprisnadikiwisnuwandana@users.noreply.github.com>
2021-03-28 01:27:58 +0700

    Add files via upload

---
+++ ComputeMethods5026201041.java
@@ -0,0 +1,27 @@
+package com.company;
+import java.util.*;
+import java.lang.*;
+
+public class ComputeMethods5026201041 {
+
+    public double fToC (double degreesF){
+        double Celcius = (double) 5/9*(degreesF-32);
+        System.out.println("Temp in celsius is " + Celcius);
+        return Celcius;
+    }
+
+    public double hypotenuse(int a, int b){
+        double Hypo = Math.sqrt(a*a + b*b);
+        System.out.println("Hypotenuse is " + Hypo);
+        return Hypo;
+    }
+
+    public int roll(){
+        Random rndNum = new Random();
+        int dice1 = rndNum.nextInt(6) + 1;
+        int dice2 = rndNum.nextInt(6) + 1;
+        int total = dice1 + dice2;
+        System.out.println("The sum of the dice values is " + total);
+        return total;
+    }
+}
